51cc7380fa1f30913a59bceb2c7036dcac727ef7
Qing Wang <chingwang.qw@gmail.com>
2019-09-15 16:16:09 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,7 @@
-# Your code here
+def meal_choice(veg1, veg2, protein = "meat")
+  if meal_choice == veg1 + veg2 
+  puts "What a nutritious meal!"
+  elsif (meal_choice == protein + veg1 + veg2)
+  puts "A plate of #{protein} with #{veg1} and #{veg2}." 
+end 
+
